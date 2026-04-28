44af1705359fc696ae33e1b77d174298646b149d
Andres-047Prueba <andresfmp149@gmail.com>
2026-04-28 17:41:58 -0500

    Update navbar.component.ts

--- digitalserv-app/src/app/shared/navbar.component.ts
+++ digitalserv-app/src/app/shared/navbar.component.ts
@@ -1,12 +1,3 @@
-// ============================================================
-// navbar.component.ts  —  Componente reutilizable del navbar
-//
-// CONCEPTOS ANGULAR:
-//   standalone: true  → no necesita NgModule, se importa directo
-//   @Input()          → propiedad que recibe el componente padre
-//   *ngFor            → repite <li> por cada enlace del arreglo
-//   [class.activo]    → agrega la clase CSS solo si la condición es true
-// ============================================================
 import { Component, Input } from '@angular/core';
 import { CommonModule }     from '@angular/common';
 
@@ -36,14 +27,9 @@ import { CommonModule }     from '@angular/common';
   `
 })
 export class NavbarComponent {
-  /*
-    @Input(): permite recibir datos del componente padre.
-    Uso: <app-navbar [paginaActual]="'servicios.html'"></app-navbar>
-    Esto es Property Binding (unidireccional: padre → hijo).
-  */
+
   @Input() paginaActual: string = 'index.html';
 
-  // Arreglo que *ngFor itera para generar los links
   enlaces = [
     { texto: 'Inicio',     ruta: 'index.html'     },
     { texto: 'Servicios',  ruta: 'servicios.html'  },
